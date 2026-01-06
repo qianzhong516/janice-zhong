afa7c5c558aaaa4f3df4388211c599b77421d02c
qianzhong516 <ads.gutless810@passinbox.com>
2026-01-06 19:28:17 +1100

    update page views

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
             Cloud Resume Challenge
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Visitor Count:{" "}
+            Page Views:{" "}
             {isLoading ? (
               <span>loading...</span>
             ) : error ? (
