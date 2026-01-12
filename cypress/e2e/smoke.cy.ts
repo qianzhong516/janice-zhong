9a199e80a6db32afc45915d014176cafdc327b4f
qianzhong516 <ads.gutless810@passinbox.com>
2026-01-12 21:01:36 +1100

    fix test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,9 +1,11 @@
 const API_DOMAIN = process.env.API_DOMAIN;
+const PATH = "visits";
+const VISITS_URL = [API_DOMAIN, PATH].join("/");
 
 describe("Smoke test", () => {
   it("loads homepage", () => {
-    cy.intercept("GET", API_DOMAIN).as("get");
-    cy.intercept("PUT", API_DOMAIN).as("put");
+    cy.intercept("GET", VISITS_URL).as("get");
+    cy.intercept("PUT", VISITS_URL).as("put");
 
     cy.visit("/");
     cy.contains("Total Views");
